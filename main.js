2d81b1451d199e3799baf27960f8c3d9ecca0615
quanghuyav <huyav1999@gmail.com>
2023-01-05 22:32:50 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const binhPhuong = (a, b) => {
+    for (let i = a; i<=b;i++){
+        console.log(i*i*i)
+    }
+}
+console.log('sử dụng hàm')
+binhPhuong(1,10)
+binhPhuong(20,30)
+
+console.log('khôngsử dụng hàm')
+
+for ( let i = 1; i<=10;i++){
+    console.log(i*i*i)
+}
+for (let i = 20; i <=30; i++){
+    console.log(i*i*i)
+}
+
+
